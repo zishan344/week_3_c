363c99419cb71e81c078c887cb8fb62e59e35c1f
zishan344 <zishanahmed344@gmail.com>
2024-04-22 10:53:02 +0600

    exploring array string

---
+++ module_10/array_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+   char a[6];
+   int length = sizeof(a)/1;
+   printf("%d\n", length);
+   for (int i = 0; i < 6; i++)
+   {
+    scanf("%c", &a[i]);
+   }
+
+   for (int i = 0; i < 6; i++)
+   {
+    printf("%c\n", a[i]);
+   }
+   
+   
+
+  return 0;
+}
