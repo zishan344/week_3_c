9b86ae36dd2049ff82814e66ae1027e4f70ec0b8
zishan344 <zishanahmed344@gmail.com>
2024-04-26 16:43:51 +0600

    complete problem3 farmer timing count

---
+++ mid_term_exam/Farmers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int mOne,mTwo,d;
+  int totalDay;
+  for (int i = 1; i <= n; i++)
+  {
+    // printf("%d ",i);
+    scanf("%d %d %d",&mOne,&mTwo,&d);
+    totalDay = ceil((double)d/(mOne + mTwo)*mTwo);
+   
+     
+    printf("%d\n", totalDay);
+  }
+  
+  return 0;
+}
